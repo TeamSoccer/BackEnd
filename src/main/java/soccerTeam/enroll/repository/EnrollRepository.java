423cc48965fd32d404254e0c913b3d964bfbc921
QQWaseokE <waseoke@gmail.com>
2024-08-27 10:23:53 +0900

    feat: 선수 list 불러오기

--- src/main/java/soccerTeam/enroll/repository/EnrollRepository.java
+++ src/main/java/soccerTeam/enroll/repository/EnrollRepository.java
@@ -1,5 +1,11 @@
 package soccerTeam.enroll.repository;
 
+import soccerTeam.team.repository.SoccerTeamEntity;
+
+import java.util.List;
+
 public interface EnrollRepository {
     EnrollEntity save(EnrollEntity enroll);
+
+    List<EnrollEntity> findByTeam(SoccerTeamEntity soccerTeam);
 }
--- src/main/java/soccerTeam/enroll/service/EnrollService.java
+++ src/main/java/soccerTeam/enroll/service/EnrollService.java
@@ -2,7 +2,13 @@ package soccerTeam.enroll.service;
 
 import soccerTeam.enroll.dto.EnrollCreateRequest;
 import soccerTeam.enroll.dto.EnrollCreateResponse;
+import soccerTeam.enroll.dto.EnrollListResponse;
+
+import java.util.List;
 
 public interface EnrollService {
     EnrollCreateResponse create(String username, EnrollCreateRequest enrollCreateRequest);
+
+    // enroll-post
+    List<EnrollListResponse> getEnrollListByTeam(Long teamId);
 }
